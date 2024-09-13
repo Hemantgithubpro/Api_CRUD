d3282887e356cf43efc3bc13bd83c98383aa7d5a
Hemant <147234335+nasahackathonnoida@users.noreply.github.com>
2024-09-13 11:07:32 +0530

    mongo edit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect("mongodb://localhost:27010/Api_CRUD", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
